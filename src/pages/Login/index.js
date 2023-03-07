960cb62aa41c2eb1b0a863cfc26df45ddec3dbcc
Henrique Hanthequeste <henriquehanthequeste@hotmail.com>
2023-03-07 19:27:59 -0300

    Change

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
+
 export default function Login() {
     const navigation = useNavigation();
 
